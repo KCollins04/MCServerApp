58a52991e6780b9e35b42344204ec75736d6e79f
Samuel Lim <samuel.lim23@d155.org>
2022-12-02 13:02:33 -0600

    Fixed pingServer function

--- MCServerApp/ViewController.swift
+++ MCServerApp/ViewController.swift
@@ -9,16 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
+    // TODO: Connect outlets to View
     @IBOutlet weak var numPlayers: UILabel!
-    @IBOutlet weak var lebelBedRock: UILabel!
-    @IBOutlet weak var lebelVersion: UILabel!
+    @IBOutlet weak var labelBedRock: UILabel!
+    @IBOutlet weak var labelVersion: UILabel!
     @IBOutlet weak var labelName: UILabel!
     @IBOutlet var imagePlayer: UIView!
     
     enum ServerReponseError: Error {
-        case invalidIp
-        case cannotReach
         case fatal
     }
     
@@ -38,37 +36,41 @@ class ViewController: UIViewController {
         }
         let online: Bool
         let status: Bool
-        let favicon_base64: String
-        let source: String
-        let took: Double
-        let cache: CacheResponse
-        let version: VersionResponse
-        let players: PlayersReponse
-        let description: String
-        let fetch: String
+        let favicon_base64: String?
+        let source: String?
+        let took: Double?
+        let cache: CacheResponse?
+        let version: VersionResponse?
+        let players: PlayersReponse?
+        let description: String?
+        let fetch: String?
+        let error: String?
     }
     
     func pingServer(_ ipAddress:String) throws -> ServerResponse {
         let requestUrl =
         // .addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!
-        "https://eu.mc-api.net/v3/server/ping/" + ipAddress
+        "https://mc-api.net/v3/server/ping/" + ipAddress
         let url = URL(string: requestUrl)
         let data = (try? Data(contentsOf: url!))!
         do {
             let res = try JSONDecoder().decode(ServerResponse.self, from: data)
-            
-            // Sets the variables to be gotten by the prepare for segue function
             return res
-            
         } catch let error {
             print(error)
-            throw ServerReponseError.cannotReach
+            throw ServerReponseError.fatal
         }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        pingServer("mc.hypixel.net").description
+        do{
+            try print(pingServer("mc.hypixel.net").status)
+            try print(pingServer("mc.doesnotexist.net").status)
+        } catch{
+            print("ERROR")
+        }
+
     }
 
 
