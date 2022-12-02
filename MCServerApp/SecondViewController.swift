243b8bc3c975d95dbc95b8ce101245368dc2062e
ok <kelsey.collins23@d155.org>
2022-12-02 08:03:20 -0600

    Main Done and control everything on story board

--- MCServerApp/SecondViewController.swift
+++ MCServerApp/SecondViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var textName: UITextField!
+    
+    @IBOutlet weak var textIPAdress: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,14 +20,11 @@ class SecondViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func switchBedRock(_ sender: UISwitch) {
+        
     }
-    */
+    @IBAction func addServer(_ sender: UIButton) {
+    }
+    
 
 }
--- MCServerApp/ViewController.swift
+++ MCServerApp/ViewController.swift
@@ -8,6 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var numPlayers: UILabel!
+    @IBOutlet weak var lebelBedRock: UILabel!
+    @IBOutlet weak var lebelVersion: UILabel!
+    @IBOutlet weak var labelName: UILabel!
+    @IBOutlet var imagePlayer: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
