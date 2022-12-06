d9afa769dfee93b7d9425d4ee25d8dfdf0da7080
ok <kelsey.collins23@d155.org>
2022-12-06 07:55:21 -0600

    switch done

--- MCServerApp/SecondViewController.swift
+++ MCServerApp/SecondViewController.swift
@@ -10,20 +10,33 @@ import UIKit
 class SecondViewController: UIViewController{
 
     @IBOutlet weak var textName: UITextField!
-    
     @IBOutlet weak var textIPAdress: UITextField!
+    
+    var n=""
+    var i=""
+    var br=false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        n = textName.text!
+        i = textIPAdress.text!
+ 
         // Do any additional setup after loading the view.
     }
     
+  
 
     @IBAction func switchBedRock(_ sender: UISwitch) {
-        
+        if sender.isOn==true{
+            br=true
+        }
+        else{
+            br=false
+        }
     }
     @IBAction func addServer(_ sender: UIButton) {
+        
+        addServerGlobal(Server(name: n, ip: i, bedrock: br))
     }
     
 
