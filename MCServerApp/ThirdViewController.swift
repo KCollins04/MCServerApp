0b34dd9f9fb50fc920892da0ace373ff5a2e1592
ok <kelsey.collins23@d155.org>
2022-12-08 08:04:03 -0600

    Fixed all the imageplayer,bedrock, name, ip, version,

--- MCServerApp/ThirdViewController.swift
+++ MCServerApp/ThirdViewController.swift
@@ -16,17 +16,37 @@ class ThirdViewController: UIViewController {
     @IBOutlet weak var motDay: UILabel!
     @IBOutlet weak var bedRock: UILabel!
     @IBOutlet weak var playerTableView: UITableView!
+    @IBOutlet weak var versionThird: UILabel!
     
     var selectedServer: ServerResponse!
+    var selectedServerInfo: Server!
     
     @IBOutlet weak var version: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let imageData = Data(base64Encoded: String((selectedServer.favicon_base64 ?? defaultIcon).dropFirst(22)))
+        if let imageData = imageData {
+            playerImage.image = UIImage(data: imageData)
+            
+            versionThird.text = selectedServer.version?.name
+            
+            ipAdress.text = selectedServerInfo.ip
+            
+            serverName.text = selectedServerInfo.name
+           
+            motDay.text = selectedServer.online ? "Online" : "Offline"
+            bedRock.text = selectedServerInfo.bedrock
+            
 
-        // Do any additional setup after loading the view.
+            
+        }
+        
+                
     }
     
+
 
     
 }
