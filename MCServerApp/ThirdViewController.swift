5b9c310e72fd60e2ef16ad350c18f797a77a56a8
ok <kelsey.collins23@d155.org>
2022-12-08 07:32:41 -0600

    ThirdViewController

---
+++ MCServerApp/ThirdViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ThirdViewController.swift
+//  MCServerApp
+//
+//  Created by KELSEY COLLINS on 12/8/22.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    
+    @IBOutlet weak var playerImage: UIImageView!
+    @IBOutlet weak var ipAdress: UILabel!
+    @IBOutlet weak var serverName: UILabel!
+    @IBOutlet weak var motDay: UILabel!
+    @IBOutlet weak var bedRock: UILabel!
+    @IBOutlet weak var playerTableView: UITableView!
+    
+    
+    @IBOutlet weak var version: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+}
