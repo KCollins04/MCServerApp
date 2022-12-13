6f0191fca6d3b78e262b51d57dde004c588f6311
Samuel Lim <samuel.lim23@d155.org>
2022-12-13 08:09:50 -0600

    Added MOTD (sort of)

--- MCServerApp/ThirdViewController.swift
+++ MCServerApp/ThirdViewController.swift
@@ -25,17 +25,30 @@ class ThirdViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let imageData = Data(base64Encoded: String((selectedServer.favicon_base64 ?? defaultIcon).dropFirst(22)))
+        let imageData = Data(base64Encoded: String((selectedServer.icon ?? defaultIcon).dropFirst(22)))
         if let imageData = imageData {
             playerImage.image = UIImage(data: imageData)
             
-            versionThird.text = String((selectedServer.version?.name ?? "REQUIRES MC UNKNOWN").dropFirst(8))
+            versionThird.text = String((selectedServer.version ?? "REQUIRES MC UNKNOWN").dropFirst(8))
             
             ipAdress.text = selectedServerInfo.ip
             
             serverName.text = selectedServerInfo.name
            
-            motDay.text = selectedServer.online ? "Online" : "Offline"
+            let string1 = NSAttributedString(string: "", attributes: [:])
+            var newMutableString = NSMutableAttributedString()
+            selectedServer.motd?.html.forEach{
+                let string2 = $0.htmlToAttributedString!
+                newMutableString = string1.mutableCopy() as! NSMutableAttributedString
+                newMutableString.append(string2)
+                print(newMutableString)
+            }
+            
+            motDay.attributedText = newMutableString.copy() as? NSAttributedString
+            
+            //motDay.attributedText = NSAttributedString(string: "html", attributes: [:])
+            
+            
             bedRock.text = String(selectedServerInfo.bedrock)
             
 
